de1019e7f7be1b3cd827fd4c910fff8a3813c22a
viralsavani <viral.savani@image-metrics.com>
2023-01-06 09:37:42 -0800

    add some debug info in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,9 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
+# TODO: Make this only visbile if user selects debug workflow run
+echo "Input Options: $INPUT_OPTIONS"
+echo "Input Run: $INPUT_RUN"
+echo "Input Shell: $INPUT_SHELL"
+
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
