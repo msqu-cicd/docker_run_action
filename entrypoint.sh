a12ed3a740890b660700d1fc3cd98280b2837a47
Christopher De Jesus <christopher.dejesus@image-metrics.com>
2023-01-18 16:40:15 -0400

    Adding --env-file docker-run-action.env from PR #23

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+env | egrep -v "^(#|;| |PATH|SHLVL|HOSTNAME|DOCKER_*)" | awk '$1 ~ /^\w+=/' | xargs -0 > docker-run-action.env
+
 if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
@@ -13,4 +15,4 @@ echo "Input Options: $INPUT_OPTIONS"
 echo "Input Run: $INPUT_RUN"
 echo "Input Shell: $INPUT_SHELL"
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run --env-file docker-run-action.env -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
